5f91046a7f15a0305785ce48320ad874b4395fbb
Saffat Hasan <saffathasan@gmail.com>
2019-09-06 22:43:31 -0400

    Added initial distance tests for range 1 and 0

---
+++ K-Closest-Points/src/main/python/driver.py
@@ -0,0 +1,4 @@
+def get_distance(p1, p2):
+    if p1 != p2:
+        return 1
+    return 0
---
+++ K-Closest-Points/src/test/python/distance_test.py
@@ -0,0 +1,22 @@
+import unittest
+from src.main.python import driver
+
+
+class KNearestPointsTest(unittest.TestCase):
+    """ Test class for Colors and PieceType representations """
+
+    def test_distance_function(self):
+        vertex = [1,2]
+        point = [1,2]
+        actual = driver.get_distance(vertex, point)
+        expected = 0
+
+        self.assertEqual(expected, actual)
+
+    def test_distance_from_1_2_to_1_3_is_1(self):
+        vertex = [1,2]
+        point = [1,3]
+        actual = driver.get_distance(vertex, point)
+        expected = 1
+
+        self.assertEqual(expected, actual)
